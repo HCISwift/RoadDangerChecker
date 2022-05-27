dbe259dfff74c7d1f8efe7ad1bd057ae8c1228cb
DaunJeong <wjdek88@naver.com>
2022-05-27 19:50:12 +0900

    Inital commit

---
+++ RoadDangerChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoadDangerChecker
+//
+//  Created by 정다운 on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
