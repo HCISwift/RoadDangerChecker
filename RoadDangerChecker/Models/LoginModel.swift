948f6609eeae1590ca9464f74ca3bb857ff8c986
steampower33 <steampower33@naver.com>
2022-06-02 17:08:12 +0900

    로그인 기능 및 로그인 후 MapContentViewController로의 이동 구현

--- RoadDangerChecker/Models/LoginModel.swift
+++ RoadDangerChecker/Models/LoginModel.swift
@@ -1,7 +1,28 @@
 //
-//  Login.swift
+//  LoginModel.swift
 //  RoadDangerChecker
 //
 //  Created by SeungMin Lee on 2022/05/30.
 //
+
+import UIKit
+
+protocol LoginModelDelegate: AnyObject {
+    func login(_ musicPlayer: LoginModel, didLoginAvailable canLogin: Bool)
+}
+
+class LoginModel {
+    weak var delegate: LoginModelDelegate?
+    
+    func loginCheck(id: String, pwd: String){
+        for user in userModel.users {
+            if user.email == id && user.password == pwd {
+                delegate?.login(self, didLoginAvailable: true)
+            } else {
+                delegate?.login(self, didLoginAvailable: false)
+            }
+        }
+    }
+}
+
 
