258523624a5eb1c48d99416f926201b13ecbecd3
steampower33 <steampower33@naver.com>
2022-06-18 13:45:15 +0900

    버튼 로직 변경

---
+++ RoadDangerChecker/RoadDangerCheckerController/SharedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SharedViewController.swift
+//  RoadDangerChecker
+//
+//  Created by SeungMin Lee on 2022/06/18.
+//
+
+import UIKit
+
+class SharedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        setupViews()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupViews() {
+        
+    }
+    
+    
+}
